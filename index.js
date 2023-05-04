cd1ff67684ca9dbf527afc3b97e1b46d1d1cf945
Yomkool <bannon.pj@gmail.com>
2023-05-03 20:52:14 -0400

    Set up proxy for requests

--- index.js
+++ index.js
@@ -1,19 +1,56 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+const yaml = require('js-yaml');
 
 const app = express();
 const PORT = 3333;
 
-app.use(bodyParser.json());
-app.use(cors());
+// Note: Setting CORS to allow chat.openapi.com is required for ChatGPT to access your plugin
+app.use(cors({ origin: [`http://localhost:${PORT}`, 'https://chat.openai.com'] }));
+app.use(express.json());
 
-// Your custom endpoints here
-app.get('/getDocs', (req, res) => {
-  // Example endpoint to return a list of todos
-  res.json({ todos: ['get groceries', 'walk the dog'] });
+const api_url = 'https://example.com';
+
+app.get('/.well-known/ai-plugin.json', (req, res) => {
+  res.sendFile(path.join(__dirname, 'ai-plugin.json'));
+});
+
+app.get('/openapi.yaml', (req, res) => {
+  const yamlData = yaml.load(fs.readFileSync(path.join(__dirname, 'openapi.yaml'), 'utf8'));
+  res.json(yamlData);
+});
+
+app.get('/openapi.json', (req, res) => {
+  res.sendFile(path.join(__dirname, 'openapi.json'));
+});
+
+app.all('/:path', async (req, res) => {
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+
+  const url = `${api_url}/${req.params.path}`;
+  console.log(`Forwarding call: ${req.method} ${req.params.path} -> ${url}`);
+
+  try {
+    const response = await axios({
+      method: req.method,
+      url,
+      headers,
+      params: req.query,
+      data: req.body,
+    });
+
+    res.send(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: 'An error occurred while forwarding the request.' });
+  }
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Proxy server listening on port ${PORT}`);
 });
